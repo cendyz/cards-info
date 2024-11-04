4cc7b53130e9eb5a1a52722b243e31e8e5eb9cd9
cendyz <arek57823@gmail.com>
2024-11-04 08:20:09 +0100

    desktop done ez

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-const cardNumbers = document.querySelector(".main__content-cards-one-texts-numbers");
+const cardNumbers = document.querySelector(".main__content-cards-one-parent-texts-numbers");
 const cardNumberInput = document.getElementById("number");
 const allInputs = [cardNumberInput];
 
 const checkCardNumber = () => {
-	
+	let value = cardNumberInput.value.replace(/[^0-9]/g, "");
+	if (value.length > 16) value = value.slice(0, 16);
+	cardNumberInput.value = value.replace(/(\d{4})(?=\d)/g, "$1 ");
+	value = value.padEnd(16, "0");
+	cardNumbers.textContent = value.replace(/(\d{4})(?=\d)/g, "$1 ");
 };
 
 cardNumberInput.addEventListener("input", () => {
